e5156a8fd0f3a361a5f0b06d50e9c93bddb0ec75
Marc ter Bogt <marc@itsburning.nl>
2023-07-06 09:42:02 +0200

    Calls to backend API depend on the current environment

--- resume-site/assets/js/visitor_counter.js
+++ resume-site/assets/js/visitor_counter.js
@@ -1,5 +1,13 @@
 const counterParagraph = document.querySelector("#visitor-counter");
-COUNTER_API = 'https://e949f5af.azurewebsites.net/api/visitor_counter?code=Y_mL9yu7qqYy-nGDSXAb8AaeJjGiwwXA9VO05Q7NaL_fAzFuo9Nr8Q==';
+if (window.location.hostname === 'resume-staging.itsburning.nl') {
+  env = 's'
+  key = 'b6ypZ3KwPN_aKIu1sBfULp5lZqKxxOUiRbuhikgv7qiPAzFuLgUFow=='
+}
+else {
+  env = 'p'
+  key = 'rW97JzPfHYVTSdUyE9ixm6QF6BrHoQwU9uNh9cPDGaTKAzFuGvyKeQ=='
+}
+COUNTER_API = `https://mtb-${env}-cv-backend-func.azurewebsites.net/api/visitor_counter?code=${key}`;
 
 const fetchVisitorCounter = async (event) => {
   const fetchedResult = await fetch(COUNTER_API, {
